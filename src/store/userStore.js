f4fc6cd1cd97be0226d817d31df79fad502c68f8
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2025-07-17 11:27:15 +0530

    modified userStore for persistent storage of user.

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,13 +1,18 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
 
-// Define the store
-const userStore = create((set) => ({
-  // State
-  user: { name: '', email: '' },
-
-  // Actions
-  setUser: (userData) => set({ user: userData }),
-  clearUser: () => set({ user: { name: '', email: '' } }),
-}));
+const userStore = create(
+  persist(
+    (set) => ({
+      user: { name: '', email: '' },
+      setUser: (userData) => set({ user: userData }),
+      clearUser: () => set({ user: { name: '', email: '' } }),
+    }),
+    {
+      name: 'zustand-store', // Key in localStorage
+      storage: createJSONStorage(() => localStorage), // Use localStorage
+    }
+  )
+);
 
 export default userStore;
